17d5e3ef2a31b355fcfd2fa06acd7bfd59f0f649
jbuelow1 <jdbuelow@gmail.com>
2021-03-31 09:52:36 -0500

    road and segments

--- src/main/java/com/jbuelow/mc/easyroad/store/Road.java
+++ src/main/java/com/jbuelow/mc/easyroad/store/Road.java
@@ -1,12 +1,19 @@
 package com.jbuelow.mc.easyroad.store;
 
+import org.bukkit.Location;
 import org.bukkit.configuration.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class Road {
 
     private final UUID uuid;
+
+    private ArrayList<RoadSegment> segments;
+
     private String name;
 
     private Road(UUID uuid) {
@@ -21,6 +28,13 @@ public class Road {
         conf.set("uuid", uuid.toString());
         conf.set("name", name);
 
+        conf.createSection("segments");
+
+        for (RoadSegment rs : segments) {
+            conf.set("segments." + rs.getSegId() + ".a", rs.getPoint1());
+            conf.set("segments." + rs.getSegId() + ".b", rs.getPoint2());
+        }
+
         return conf;
     }
 
@@ -28,10 +42,52 @@ public class Road {
         Road r = new Road(UUID.fromString(conf.get("uuid").toString()));
         r.name = conf.get("name").toString();
 
+        for (Object segkey: conf.getList("segments")) {
+            String s = (String) segkey;
+
+            Location p1 = conf.getLocation("segments." + s + ".a");
+            Location p2 = conf.getLocation("segments." + s + ".b");
+
+            RoadSegment seg = new RoadSegment() {
+                @Override
+                public Location getPoint1() {
+                    return p1;
+                }
+
+                @Override
+                public Location getPoint2() {
+                    return p2;
+                }
+
+                @Override
+                public Road getRoad() {
+                    return r;
+                }
+
+                @Override
+                public String getSegId() {
+                    return s;
+                }
+            };
+
+            r.segments.add(seg);
+        }
         return r;
     }
 
     public UUID getUUID() {
         return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<RoadSegment> getSegments() {
+        return segments;
     }
 }
--- src/main/java/com/jbuelow/mc/easyroad/store/RoadSegment.java
+++ src/main/java/com/jbuelow/mc/easyroad/store/RoadSegment.java
@@ -2,8 +2,16 @@ package com.jbuelow.mc.easyroad.store;
 
 import org.bukkit.Location;
 
+import java.util.UUID;
+
 public abstract class RoadSegment {
+
     public abstract Location getPoint1();
     public abstract Location getPoint2();
     public abstract Road getRoad();
+    public abstract String getSegId();
+
+    public String getID() {
+        return getRoad().getUUID().toString() + "#" + getSegId();
+    }
 }
