6ff36c198fa53be4b263849388cd05da7fc9c709
YammyShep <jdbuelow@gmail.com>
2021-04-14 09:57:45 -0500

    added color scheme constants file

---
+++ src/main/java/com/jbuelow/mc/easyroad/conf/ColorScheme.java
@@ -0,0 +1,13 @@
+package com.jbuelow.mc.easyroad.conf;
+
+import org.bukkit.ChatColor;
+
+public abstract class ColorScheme {
+
+    public static ChatColor TEXT_DEFAULT = ChatColor.BLUE;
+    public static ChatColor TEXT_ERROR = ChatColor.RED;
+    public static ChatColor TEXT_EMPHASIS = ChatColor.YELLOW;
+    public static ChatColor TEXT_LOWEMPHASIS = ChatColor.GRAY;
+    public static ChatColor TEXT_INSIGNIFICANT = ChatColor.DARK_GRAY;
+
+}
